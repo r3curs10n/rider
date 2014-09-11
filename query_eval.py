552b533139d39b701d1ea9660af03a2fbb4fc5b5
r3curs10n <shreyasrulez@gmail.com>
2014-09-11 20:38:13 +0530

    almost done

---
+++ query_eval.py
@@ -0,0 +1,101 @@
+from tokenizem import base, is_stop_word
+import tf_score
+
+tokens = (
+    'TERM','PHRASE',
+    'OR','AND','LP','RP',
+    'NOT',
+    )
+
+# Tokens
+
+t_OR = r'\|'
+t_AND = r'\*'
+t_LP = r'\('
+t_RP = r'\)'
+t_NOT = r'\!'
+
+def t_PHRASE(t):
+    r'\".*?\"'
+    t.value = map(base, t.value[1:-1].lower().split())
+    return t
+
+def t_TERM(t):
+    r'[^()" \t!|*]+'
+    t.value = base(t.value.lower())
+    print t.value
+    return t
+
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+    
+# Build the lexer
+import ply.lex as lex
+lex.lex()
+
+# Parsing rules
+
+precedence = (
+    ('left','OR'),
+    ('left','AND'),
+    )
+
+# dictionary of names
+names = { }
+
+def p_query_term(t):
+    'query : TERM'
+    t[0] = tf_score.get_scored_list(t[1])
+
+def p_query_phrase(t):
+    'query : PHRASE'
+    t[0] = tf_score.phrasal(t[1])
+
+def p_query_par(t):
+    'query : LP query RP'
+    t[0] = t[2]
+
+def p_query_bool(t):
+    '''query : query AND query
+             | query OR query
+             | query NOT query'''
+    if t[2] == '*':
+        t[0] = tf_score.merge_and(t[1], t[3])
+        print 'AND called'
+    elif t[2] == '|':
+        t[0] = tf_score.merge_or(t[1], t[3])
+        print 'OR called'
+    else:
+        t[0] = tf_score.merge_not(t[1], t[3])
+
+def p_error(t):
+    print("Syntax error at '%s'" % t.value)
+
+import ply.yacc as yacc
+yacc.yacc()
+
+def query_eval(query):
+    query = query.lower()
+    res = sorted(yacc.parse(query), key=lambda x: -x['score'])
+    resd = []
+    for r in res:
+        resd.append('(<a href="file:///home/shreyas/code/rider/%d/%d">%d</a>, %f)' % (r['d']//10000, r['d'], r['d'], r['score']))
+    return '<br>'.join(resd)
+
+if __name__ == '__main__':
+    while 1:
+        try:
+            s = raw_input('query > ')   # Use raw_input on Python 2
+        except EOFError:
+            break
+        #res = sorted(yacc.parse(s), key=lambda x: -x['score'])[:5]
+        #print res
+        #tf_score.open_doc(res[0]['d'])
+        print query_eval(s)
---
+++ search_api.py
@@ -0,0 +1,56 @@
+#Copyright Jon Berg , turtlemeat.com
+
+import string,cgi,time
+from os import curdir, sep
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import urlparse
+import query_eval
+#import pri
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        #try:
+        if 'search' not in self.path:
+        	return
+    	qs = urlparse.urlparse(self.path)
+    	print self.path
+    	args = urlparse.parse_qs(qs.query)
+    	print args
+    	self.send_response(200)
+    	self.send_header('Content-type', 'text/html')
+    	self.end_headers()
+    	self.wfile.write(query_eval.query_eval(args['q'][0]))
+    	return    
+    	#except Exception, e:
+        self.send_error(500,'Fuck, something went wrong!<br>' + e.strerr)
+     
+
+    def do_POST(self):
+        global rootnode
+        try:
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                query=cgi.parse_multipart(self.rfile, pdict)
+            self.send_response(301)
+            
+            self.end_headers()
+            upfilecontent = query.get('upfile')
+            print "filecontent", upfilecontent[0]
+            self.wfile.write("<HTML>POST OK.<BR><BR>");
+            self.wfile.write(upfilecontent[0]);
+            
+        except :
+            pass
+
+def main():
+    try:
+        server = HTTPServer(('', 8090), MyHandler)
+        print 'started httpserver...'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '^C received, shutting down server'
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
